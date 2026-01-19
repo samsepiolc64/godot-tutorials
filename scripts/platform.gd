7653b8838b98b7ecc483f04fafab3167c6ce33c6
samsepiolc64 <mjarmuzek@wp.pl>
2026-01-19 20:31:58 +0100

    Change platform script

---
+++ scripts/platform.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+const SPEED = 40
+
+var direction = -2
+
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if ray_cast_right.is_colliding():
+		direction = -2
+	if ray_cast_left.is_colliding():
+		direction = 2
+	position.x += direction * SPEED * delta
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 140.0
 const JUMP_VELOCITY = -300.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
